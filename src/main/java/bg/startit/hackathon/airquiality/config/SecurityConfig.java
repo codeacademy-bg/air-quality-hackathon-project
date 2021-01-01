313a5a8a99f4334b84eb4e76e987a3d12d7bda8a
Valentin Valchev <v_valchev@developer.bg>
2021-01-01 22:45:26 +0200

    removed unused import

--- src/main/java/bg/startit/hackathon/airquiality/config/SecurityConfig.java
+++ src/main/java/bg/startit/hackathon/airquiality/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package bg.startit.hackathon.airquiality.config;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;

cbdba3eedd807665405c4f8700e1cb344d879fb0
Valentin Valchev <v_valchev@developer.bg>
2021-01-17 16:29:22 +0200

    When deleting the old entries from the DB an exception occurs, that there is no EntityManager. This problem never occurs with the build-in H2 DB, that's used in developers mode. However, when deployed to Heroku this problem keeps coming. I've tried to fix it, but .. quick and dirty fixes do not work with PostgreSQL. So this time it's made the right way:
    1. The service contains all the logic and is annotated as @Transactional (so every method runs in transaction)
    2. The scheduled tasks are now in a separate service - calling back the AiqQualityService for the business logic.

--- src/main/java/bg/startit/hackathon/airquiality/AirquialityApplication.java
+++ src/main/java/bg/startit/hackathon/airquiality/AirquialityApplication.java
@@ -1,11 +1,13 @@
 package bg.startit.hackathon.airquiality;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@EnableAsync
+@EnableTransactionManagement
 @EnableScheduling
 @SpringBootApplication
 public class AirquialityApplication {
